ae01c97ca9e5e1cc5979f1b36d4c8591170c4a30
David P. Steelman <dsteelma@umd.edu>
2020-01-14 14:04:15 -0500

    LIBSEARCH-34. Updated "dummy" testing app to Rails 5.2.4.1
    https://issues.umd.edu/browse/LIBSEARCH-34

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -4,16 +4,17 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 require 'quick_search'
-require 'quick_search/library_website_searcher'
+require "quick_search/library_website_searcher"
 
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
 
